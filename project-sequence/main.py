100aba653c80954e47cd8088a71bea1399f4f4df
Chirayu-N <chirayu.nimonkar@gmail.com>
2024-06-03 19:11:45 -0700

    Implement Project class basics

--- project-sequence/main.py
+++ project-sequence/main.py
@@ -1,3 +1,4 @@
+
 class Task:
     def __init__(self, id: int, name: str, duration: float) -> None:
         self.id = id
@@ -12,8 +13,26 @@ class Task:
 
         self.slack = 0
     
-    def add_dependency(self, task_id) -> None:
+    def add_dependency(self, task_id: int) -> None:
         self.dependencies.append(task_id)
+
+
+class Project:
+    def __init__(self) -> None:
+        self.tasks = dict()  
+    
+
+    def add_task(self, task: Task):
+        self.tasks[task.id] = task
+    
+
+    def add_dependency(self, task_id: int, dependency_id: int):
+        self.tasks[task_id].add_dependency(dependency_id)
+    
+
+    def find_critical_path(self):
+        pass # TODO
+
 
 if __name__ == '__main__':
     task = Task(0, "Task", 10.0)
